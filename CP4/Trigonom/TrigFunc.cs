e8bb5da9e76b3ce8da4755a8ba0d356afb6ee597
su <su@209-6>
2021-09-22 17:40:46 +0300

    Добавьте файлы проекта.

---
+++ CP4/Trigonom/TrigFunc.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP4.Trigonom
+{
+    public class TrigFunc
+    {
+        public static double Factorial(uint num)
+        {
+            if (num <= 1)
+            {
+                return 1d;
+            }
+
+            return num * Factorial(num - 1);
+        }
+
+        /// <summary>
+        /// Рекурсивный метод возведения в степень
+        /// </summary>
+        /// <param name="num">Число</param>
+        /// <param name="pow">Степень</param>
+        /// <returns>Результат</returns>
+        public static double Power(double num, int pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+
+            return num * Power(num, pow - 1);
+        }
+
+        /// <summary>
+        /// Модуль числа
+        /// </summary>
+        /// <param name="num">Число</param>
+        /// <returns>Абсолютное значение числа</returns>
+        public static double Abs(double num)
+        {
+            if (num >= 0)
+            {
+                return num;
+            }
+
+            return -num;
+        }
+
+        /// <summary>
+        /// Рекурсивный метод вычисления sin(x)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <param name="precision">Точность</param>
+        /// <returns>Результат</returns>
+        public static double Sin(double x, int n = 0, double precision = 1e-10)
+        {
+            var t = Power(-1, n) * Power(x, 2 * n + 1) / Factorial((uint)(2 * n + 1));
+            if (Abs(t) < precision)
+            {
+                return t;
+            }
+
+            return t + Sin(x, n + 1, precision);
+        }
+        public static double Cos(double x, int n = 0, double precision = 1e-10)
+        {
+            var t = Power(-1, n) * Power(x, 2 * n) / Factorial((uint)(2 * n));
+            if (Abs(t) < precision)
+            {
+                return t;
+            }
+
+            return t + Cos(x, n + 1, precision);
+        }
+        public double CoSec(double x)
+        {
+            return 1 / Sin(x);
+        }
+        public static double CoTan(double x)
+        {
+            return Cos(x) / Sin(x);
+        }
+        private static double Sum(double x, int n = 1, double precision = 1e-10)
+        {
+            var current = Power(x, n) / n;
+            if (current < precision)
+            {
+                return current;
+            }
+
+            return current + Sum(x, n + 1, precision);
+        }
+
+        /// <summary>
+        /// Метод вычисления ln(1-x)
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="precision">Точность</param>
+        /// <returns>Результат</returns>
+        public static double Lnxx(double x, double precision = 1e-10)
+        {
+            return -Sum(x, precision: precision);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CP4.Trigonom;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFactorial()
+        {
+            Assert.AreEqual(24, TrigFunc.Factorial(4));
+        }
+        [TestMethod]
+        public void TestPower()
+        {
+            Assert.AreEqual(25, TrigFunc.Power(5, 2));
+        }
+        [TestMethod]
+        public void TestAbs()
+        {
+            Assert.AreEqual(24, TrigFunc.Abs(24));
+        }
+
+
+            [TestMethod]
+        public void TestSin()
+        {
+           Assert.AreEqual(Math.Round(Math.Sin(60 * Math.PI / 180), 4), Math.Round(TrigFunc.Sin(60 * Math.PI / 180), 4));
+        }
+        [TestMethod]
+        public void TestCos()
+        {
+            Assert.AreEqual(Math.Round(Math.Cos(60 * Math.PI / 180), 4), Math.Round(TrigFunc.Cos(60 * Math.PI / 180), 4));
+        }
+        [TestMethod]
+        public void TestCoSec()
+        {
+            Assert.AreEqual(Math.Round(1 / Math.Sin(60 * Math.PI / 180), 4), Math.Round(1/TrigFunc.Sin(60 * Math.PI / 180), 4));
+        }
+        [TestMethod]
+        public void TestCoTan()
+        {
+            Assert.AreEqual(Math.Round(1/Math.Tan(60 * Math.PI / 180), 4), Math.Round(TrigFunc.CoTan(60 * Math.PI / 180), 4));
+        }
+        [TestMethod]
+        public void TestSum()
+        {
+            Assert.AreEqual(24, TrigFunc.Factorial(4));
+        }
+        [TestMethod]
+        public void TestLnxx()
+        {
+            Assert.AreEqual(Math.Round(Math.Log(1 - Math.E), 4), Math.Round(TrigFunc.Lnxx(1 - Math.E), 4));
+        }
+    }
+}
